88c67aef7922de53ad93b6506075f41df8006636
TengFoongLAM <tengfoonglam@yahoo.com.sg>
2021-04-23 01:54:54 +0200

    Added sl sensor visualise

---
+++ core/sl_sensor_visualise/src/pose_stamped_to_path.cpp
@@ -0,0 +1,26 @@
+#include "sl_sensor_visualise/pose_stamped_to_path.hpp"
+
+namespace sl_sensor
+{
+namespace visualise
+{
+PoseStampedToPath::PoseStampedToPath(ros::NodeHandle nh_public, ros::NodeHandle nh_private)
+{
+  nh_private.param<std::string>("subscribe_topic", subscribe_topic_, subscribe_topic_);
+  nh_private.param<std::string>("publish_topic", publish_topic_, publish_topic_);
+
+  std::cout << subscribe_topic_ << std::endl;
+  std::cout << publish_topic_ << std::endl;
+
+  pub_ = nh_public.advertise<nav_msgs::Path>(publish_topic_, 5);
+  sub_ = nh_public.subscribe(subscribe_topic_, 5, &PoseStampedToPath::Callback, this);
+}
+
+void PoseStampedToPath::Callback(const geometry_msgs::PoseStamped& pose_stamped)
+{
+  path_.header = pose_stamped.header;
+  path_.poses.push_back(pose_stamped);
+  pub_.publish(path_);
+}
+}  // namespace visualise
+}  // namespace sl_sensor
