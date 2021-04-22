c7a3bf035876db27c337e9effbdbfe36f6fe24e5
TengFoongLAM <tengfoonglam@yahoo.com.sg>
2021-04-22 23:02:38 +0200

    Addded pc registration algo base class

---
+++ core/sl_sensor_registration/include/sl_sensor_registration/point_cloud_registration_algorithm.hpp
@@ -0,0 +1,79 @@
+#pragma once
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <Eigen/Core>
+#include "ros/ros.h"
+
+namespace sl_sensor
+{
+namespace registration
+{
+/**
+ * @brief Base class for point cloud registration algorithms
+ *
+ */
+class PointCloudRegistrationAlgorithm
+{
+public:
+  /**
+   * @brief Get settings from ROS node
+   *
+   * @param nh
+   */
+  virtual void LoadSettingsFromROSNodeHandle(ros::NodeHandle nh);
+
+  virtual void Init(){};
+
+  /**
+   * @brief Register point cloud (XYZ Points)
+   *
+   * @param reference_pc - Point cloud to be registered
+   * @param guess - initial guess transform
+   */
+  virtual void RegisterPointCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr reference_pc,
+                                  const Eigen::Matrix4f& guess = Eigen::Matrix4f::Identity()){};
+
+  /**
+   * @brief Register point cloud (XYZRGB points). By default, will convert point cloud to XYZ format and register
+   *
+   * @param reference_pc - Point cloud to be registered
+   * @param guess - initial guess transform
+   */
+  virtual void RegisterPointCloud(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr reference_pc,
+                                  const Eigen::Matrix4f& guess = Eigen::Matrix4f::Identity());
+
+  /**
+   * @brief Get last computed relative transform
+   *
+   * @return Eigen::Matrix4f
+   */
+  Eigen::Matrix4f GetRelativeTransform();
+
+  /**
+   * @brief Get abslolute transform
+   *
+   * @return Eigen::Matrix4f
+   */
+  Eigen::Matrix4f GetAbsoluteTransform();
+
+  /**
+   * @brief Set a particular settings
+   *
+   * @param setting_name
+   * @param value
+   */
+  void SetSetting(const std::string& setting_name, double value);
+
+  virtual void Close(){};
+
+  void PrintSettings();
+
+protected:
+  Eigen::Matrix4f absolute_transform_ = Eigen::Matrix4f::Identity();
+  Eigen::Matrix4f relative_transform_ = Eigen::Matrix4f::Identity();
+  std::unordered_map<std::string, double> settings_ = {};
+};
+}  // namespace registration
+
+}  // namespace sl_sensor
---
+++ core/sl_sensor_registration/src/point_cloud_registration_algorithm.cpp
@@ -0,0 +1,51 @@
+#include "sl_sensor_registration/point_cloud_registration_algorithm.hpp"
+
+namespace sl_sensor
+{
+namespace registration
+{
+void PointCloudRegistrationAlgorithm::LoadSettingsFromROSNodeHandle(ros::NodeHandle nh)
+{
+  for (auto& [setting_name, value] : settings_)
+  {
+    auto default_value = value;
+    nh.param<double>(setting_name, settings_[setting_name], default_value);
+  }
+};
+
+void PointCloudRegistrationAlgorithm::RegisterPointCloud(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr reference_pc,
+                                                         const Eigen::Matrix4f& guess)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr input_xyz_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::copyPointCloud(*reference_pc, *input_xyz_ptr);
+  RegisterPointCloud(input_xyz_ptr, guess);
+};
+
+Eigen::Matrix4f PointCloudRegistrationAlgorithm::GetRelativeTransform()
+{
+  return relative_transform_;
+};
+
+Eigen::Matrix4f PointCloudRegistrationAlgorithm::GetAbsoluteTransform()
+{
+  return absolute_transform_;
+};
+
+void PointCloudRegistrationAlgorithm::SetSetting(const std::string& setting_name, double value)
+{
+  settings_[setting_name] = value;
+}
+
+void PointCloudRegistrationAlgorithm::PrintSettings()
+{
+  std::cout << "Settings: " << std::endl;
+
+  for (auto& [setting_name, value] : settings_)
+  {
+    std::cout << "\t" << setting_name << ": " << value << std::endl;
+  }
+};
+
+}  // namespace registration
+
+}  // namespace sl_sensor
