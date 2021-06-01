a730c32d79d96a588d9dba29e97f286327ab62fb
TengFoongLAM <tengfoonglam@yahoo.com.sg>
2021-06-01 17:02:59 +0200

    Breaking frame grabber into smaller classes and place it into sl_sensor_image_acquisition: ImageGrouper done

---
+++ core/sl_sensor_image_acquisition/include/sl_sensor_image_acquisition/image_grouper.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <boost/thread/thread.hpp>
+#include <string>
+#include <vector>
+
+namespace sl_sensor
+{
+namespace image_acquisition
+{
+class ImageGrouper
+{
+public:
+  ImageGrouper(std::string image_sub_topic = "/image", int number_images_per_group = 1,
+               double image_trigger_period = 0.01, double lower_bound_tol = 0.01, double upper_bound_tol = 0.01);
+
+  void Init(ros::NodeHandle nh);
+
+  void ImageCb(const sensor_msgs::ImageConstPtr& image_ptr);
+
+  void Start();
+
+  void Stop();
+
+  void SetToleranceBounds(double lower_tol, double upper_tol);
+
+  void SetImagesPerGroup(int number_images_per_group);
+
+  void SetImageTriggerPeriod(double trigger_period);
+
+  bool RetrieveImageGroup(const ros::Time& projector_time, std::vector<sensor_msgs::Image>& result_image_vec);
+
+private:
+  ros::NodeHandle nh_;
+  ros::Subscriber image_sub_;
+
+  boost::mutex mutex_;
+
+  std::string image_sub_topic_;
+
+  std::vector<sensor_msgs::ImageConstPtr> image_ptr_buffer_ = {};
+
+  bool is_running_ = false;
+  int number_images_per_group_;
+  double image_trigger_period_;
+  double lower_bound_tol_;
+  double upper_bound_tol_;
+
+  void ClearAllImagesFromBufferBeforeTiming(const ros::Time& target_time);
+
+  bool GetImagePtrFromBuffer(const ros::Time& target_time, sensor_msgs::ImageConstPtr& image_it);
+};
+}  // namespace image_acquisition
+}  // namespace sl_sensor
---
+++ core/sl_sensor_image_acquisition/src/image_grouper.cpp
@@ -0,0 +1,169 @@
+#include "sl_sensor_image_acquisition/image_grouper.hpp"
+
+namespace sl_sensor
+{
+namespace image_acquisition
+{
+ImageGrouper::ImageGrouper(std::string image_sub_topic, int number_images_per_group, double image_trigger_period,
+                           double lower_bound_tol, double upper_bound_tol)
+  : image_sub_topic_(image_sub_topic)
+  , number_images_per_group_(number_images_per_group)
+  , image_trigger_period_(image_trigger_period)
+  , lower_bound_tol_(lower_bound_tol)
+  , upper_bound_tol_(upper_bound_tol)
+{
+}
+
+void ImageGrouper::Init(ros::NodeHandle nh)
+{
+  nh_ = nh;
+  image_sub_ = nh_.subscribe(image_sub_topic_, 10, &ImageGrouper::ImageCb, this);
+};
+
+void ImageGrouper::ImageCb(const sensor_msgs::ImageConstPtr& image_ptr)
+{
+  boost::mutex::scoped_lock lock(mutex_);
+  if (is_running_)
+  {
+    image_ptr_buffer_.emplace_back(image_ptr);
+  }
+}
+
+void ImageGrouper::Start()
+{
+  boost::mutex::scoped_lock lock(mutex_);
+  is_running_ = true;
+};
+
+void ImageGrouper::Stop()
+{
+  boost::mutex::scoped_lock lock(mutex_);
+  is_running_ = false;
+  image_ptr_buffer_.clear();
+};
+
+void ImageGrouper::SetToleranceBounds(double lower_tol, double upper_tol)
+{
+  lower_bound_tol_ = lower_tol;
+  upper_bound_tol_ = upper_tol;
+};
+
+void ImageGrouper::SetImagesPerGroup(int number_images_per_group)
+{
+  number_images_per_group_ = number_images_per_group;
+};
+
+void ImageGrouper::SetImageTriggerPeriod(double trigger_period)
+{
+  image_trigger_period_ = trigger_period;
+};
+
+bool ImageGrouper::RetrieveImageGroup(const ros::Time& projector_time,
+                                      std::vector<sensor_msgs::Image>& result_image_vec)
+{
+  bool success = false;
+
+  boost::mutex::scoped_lock lock(mutex_);
+
+  // If not enough projector timings / images we do not attempt any matching
+  if (!is_running_ || (int)image_ptr_buffer_.size() < number_images_per_group_)
+  {
+    return false;
+  }
+
+  std::vector<sensor_msgs::ImageConstPtr> temp_img_ptr_vec = {};
+  ros::Time successful_projector_time;
+
+  temp_img_ptr_vec.clear();
+
+  // We check if we can retrieve all images that make up a frame for the current projector time
+  for (int i = 0; i < number_images_per_group_; i++)
+  {
+    auto target_time = projector_time + ros::Duration(i * image_trigger_period_);
+
+    sensor_msgs::ImageConstPtr temp_ptr;
+
+    bool image_acquisition_successful = GetImagePtrFromBuffer(target_time, temp_ptr);
+
+    if (image_acquisition_successful)
+    {
+      // Successful frame acquisition, store the image pointer in temp_img_ptr_vec
+      temp_img_ptr_vec.push_back(temp_ptr);
+      continue;
+    }
+    else
+    {
+      // If not successful, we stop the looking for next image
+      // std::cout << "Unsuccessful at the " << i + 1 << "th image" << std::endl;
+    }
+  }
+
+  // if successful (all image pointer for a frame in temp_img_ptr_vec)
+  if ((int)temp_img_ptr_vec.size() == number_images_per_group_)
+  {
+    success = true;
+  }
+
+  // Fill in result image vector
+  if (success)
+  {
+    // Populate output vector
+    result_image_vec.clear();
+    for (const auto img_ptr : temp_img_ptr_vec)
+    {
+      auto& img = *img_ptr;
+      result_image_vec.emplace_back(std::move(img));
+    }
+
+    // Clear all images before and including this frame
+    ClearAllImagesFromBufferBeforeTiming(temp_img_ptr_vec.back()->header.stamp);
+  }
+
+  return success;
+}
+
+void ImageGrouper::ClearAllImagesFromBufferBeforeTiming(const ros::Time& target_time)
+{
+  int counter = 0;
+
+  for (auto it = image_ptr_buffer_.begin(); it != image_ptr_buffer_.end(); it++)
+  {
+    if (((*it)->header.stamp - target_time).toSec() <= 0.0f)
+    {
+      image_ptr_buffer_.erase(it--);
+      counter++;
+    }
+    else
+    {
+      break;
+    }
+  }
+}
+
+bool ImageGrouper::GetImagePtrFromBuffer(const ros::Time& target_time, sensor_msgs::ImageConstPtr& image_it)
+{
+  bool result = false;
+
+  for (const auto img_ptr : image_ptr_buffer_)
+  {
+    double delta_t = (img_ptr->header.stamp - target_time).toSec();
+    if (delta_t >= -1.0 * lower_bound_tol_ && delta_t <= upper_bound_tol_)
+    {
+      // If we find a image that satisfies the requirement, write the pointer to image_ptr and break the loop
+      image_it = img_ptr;
+      result = true;
+      break;
+    }
+    else if (delta_t > upper_bound_tol_)
+    {
+      // If image is in the future of target time, we stop looking
+      result = false;
+      break;
+    }
+  }
+
+  return result;
+}
+
+}  // namespace image_acquisition
+}  // namespace sl_sensor
