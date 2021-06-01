94419949228772f942d0b7566f7303b6871668b5
TengFoongLAM <tengfoonglam@yahoo.com.sg>
2021-06-01 17:24:16 +0200

    Added frame logger to frame acquisition

---
+++ core/sl_sensor_image_acquisition/include/sl_sensor_image_acquisition/image_array_logger_nodelet.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+#include <ros/ros.h>
+#include <boost/thread/thread.hpp>
+
+#include "sl_sensor_image_acquisition/ImageArray.h"
+
+namespace sl_sensor
+{
+namespace image_acquisition
+{
+class ImageArrayLoggerNodelet : public nodelet::Nodelet
+{
+public:
+  ImageArrayLoggerNodelet();
+
+private:
+  ros::Subscriber image_array_sub_;
+  std::string image_array_sub_topic_ = "/image_array_receive";
+  std::string log_directory_ = "";
+  std::string file_header_ = "";
+  const std::string file_format_ = ".bmp";
+
+  ros::NodeHandle nh_;
+  ros::NodeHandle private_nh_;
+
+  virtual void onInit();
+
+  void ImageArrayCb(const sl_sensor_image_acquisition::ImageArrayConstPtr image_arr_ptr);
+};
+
+}  // namespace image_acquisition
+}  // namespace sl_sensor
+
+PLUGINLIB_EXPORT_CLASS(sl_sensor::image_acquisition::ImageArrayLoggerNodelet, nodelet::Nodelet);
