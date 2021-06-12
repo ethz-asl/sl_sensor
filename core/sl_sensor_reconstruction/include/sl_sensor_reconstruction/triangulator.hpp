ca033962b9509526b9284dfa1b534cd2352438bc
TengFoongLAM <tengfoonglam@yahoo.com.sg>
2021-06-12 12:40:19 +0200

    Created sl_sensor_reconstruction subpackage and added Triangulator to it

---
+++ core/sl_sensor_reconstruction/include/sl_sensor_reconstruction/triangulator.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <sl_sensor_calibration/calibration_data.hpp>
+#include <vector>
+
+namespace sl_sensor
+{
+namespace reconstruction
+{
+class Triangulator
+{
+public:
+  Triangulator(calibration::CalibrationData calibration);
+  calibration::CalibrationData GetCalibrationData();
+  void Triangulate(const cv::Mat &up, const cv::Mat &vp, const cv::Mat &mask, const cv::Mat &shading,
+                   cv::Mat &point_cloud);
+
+private:
+  void TriangulateFromUp(const cv::Mat &up, cv::Mat &xyz);
+  void TriangulateFromVp(const cv::Mat &vp, cv::Mat &xyz);
+  void TriangulateFromUpVp(const cv::Mat &up, const cv::Mat &vp, cv::Mat &xyz);
+  calibration::CalibrationData calibration_data_;
+  cv::Mat determinant_tensor_;
+  cv::Mat uc_, vc_;
+  cv::Mat Pp_, Pc_;
+  cv::Mat lens_map_1_, lens_map_2_;
+  cv::Mat proj_points_cam_;
+  std::vector<cv::Mat> xyzw_precompute_offset_;
+  std::vector<cv::Mat> xyzw_precompute_factor_;
+  int number_pixels_;
+};
+
+}  // namespace reconstruction
+
+}  // namespace sl_sensor
---
+++ core/sl_sensor_reconstruction/src/triangulator.cpp
@@ -0,0 +1,192 @@
+#include <math.h>
+#include <iostream>
+#include "sl_sensor_reconstruction/triangulator.hpp"
+
+using namespace sl_sensor::calibration;
+
+namespace sl_sensor
+{
+namespace reconstruction
+{
+CalibrationData Triangulator::GetCalibrationData()
+{
+  return calibration_data_;
+}
+
+Triangulator::Triangulator(CalibrationData calibration) : calibration_data_(calibration)
+{
+  // Precompute uc_, vc_ maps
+  uc_.create(calibration_data_.frame_height_, calibration_data_.frame_width_, CV_32F);
+  vc_.create(calibration_data_.frame_height_, calibration_data_.frame_width_, CV_32F);
+
+  for (unsigned int row = 0; row < (unsigned int)calibration_data_.frame_height_; row++)
+  {
+    for (unsigned int col = 0; col < (unsigned int)calibration_data_.frame_width_; col++)
+    {
+      uc_.at<float>(row, col) = col;
+      vc_.at<float>(row, col) = row;
+    }
+  }
+
+  // Precompute determinant tensor
+  Pc_ = cv::Mat(3, 4, CV_32F, cv::Scalar(0.0));
+  cv::Mat(calibration_data_.Kc_).copyTo(Pc_(cv::Range(0, 3), cv::Range(0, 3)));
+
+  Pp_ = cv::Mat(3, 4, CV_32F);
+  cv::Mat temp(3, 4, CV_32F);
+  cv::Mat(calibration_data_.Rp_).copyTo(temp(cv::Range(0, 3), cv::Range(0, 3)));
+  cv::Mat(calibration_data_.Tp_).copyTo(temp(cv::Range(0, 3), cv::Range(3, 4)));
+  Pp_ = cv::Mat(calibration_data_.Kp_) * temp;
+
+  cv::Mat e = cv::Mat::eye(4, 4, CV_32F);
+
+  int sz[] = { 4, 3, 3, 3 };
+  cv::Mat C(4, sz, CV_32F, cv::Scalar::all(0));
+  for (int k = 0; k < 4; k++)
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 3; j++)
+      {
+        for (int l = 0; l < 3; l++)
+        {
+          cv::Mat op(4, 4, CV_32F);
+          Pc_.row(i).copyTo(op.row(0));
+          Pc_.row(j).copyTo(op.row(1));
+          Pp_.row(l).copyTo(op.row(2));
+          e.row(k).copyTo(op.row(3));
+          C.at<float>(cv::Vec4i(k, i, j, l)) = cv::determinant(op.t());
+        }
+      }
+    }
+  }
+  determinant_tensor_ = C;
+
+  // Precompute lens correction maps
+  cv::Mat eye = cv::Mat::eye(3, 3, CV_32F);
+  cv::initUndistortRectifyMap(calibration_data_.Kc_, calibration_data_.Kc_, eye, calibration_data_.Kc_,
+                              cv::Size(calibration_data_.frame_width_, calibration_data_.frame_height_), CV_16SC2,
+                              lens_map_1_, lens_map_2_);
+
+  // Precompute parts of xyzw
+  xyzw_precompute_offset_.resize(4);
+  xyzw_precompute_factor_.resize(4);
+  for (unsigned int i = 0; i < 4; i++)
+  {
+    xyzw_precompute_offset_[i] = C.at<float>(cv::Vec4i(i, 0, 1, 0)) - C.at<float>(cv::Vec4i(i, 2, 1, 0)) * uc_ -
+                                 C.at<float>(cv::Vec4i(i, 0, 2, 0)) * vc_;
+    xyzw_precompute_factor_[i] = -C.at<float>(cv::Vec4i(i, 0, 1, 2)) + C.at<float>(cv::Vec4i(i, 2, 1, 2)) * uc_ +
+                                 C.at<float>(cv::Vec4i(i, 0, 2, 2)) * vc_;
+  }
+
+  // For UpVpTriangulate
+  int number_pixels_ = uc_.rows * uc_.cols;
+  proj_points_cam_ = cv::Mat(2, number_pixels_, CV_32F);
+  uc_.reshape(0, 1).copyTo(proj_points_cam_.row(0));
+  vc_.reshape(0, 1).copyTo(proj_points_cam_.row(1));
+}
+
+void Triangulator::Triangulate(const cv::Mat &up, const cv::Mat &vp, const cv::Mat &mask, const cv::Mat &shading,
+                               cv::Mat &point_cloud)
+{
+  cv::Mat up_to_triangulate;
+  cv::Mat vp_to_triangulate;
+  cv::Mat mask_to_triangulate;
+  cv::Mat shading_to_triangulate;
+
+  // Undistort up, mask and shading
+  if (!up.empty())
+  {
+    cv::remap(up, up_to_triangulate, lens_map_1_, lens_map_2_, cv::INTER_LINEAR);
+  }
+  if (!vp.empty())
+  {
+    cv::remap(vp, vp_to_triangulate, lens_map_1_, lens_map_2_, cv::INTER_LINEAR);
+  }
+
+  cv::remap(mask, mask_to_triangulate, lens_map_1_, lens_map_2_, cv::INTER_LINEAR);
+  cv::remap(shading, shading_to_triangulate, lens_map_1_, lens_map_2_, cv::INTER_LINEAR);
+
+  // Triangulate
+  cv::Mat xyz;
+  if (!up.empty() && vp.empty())
+    TriangulateFromUp(up_to_triangulate, xyz);
+  else if (!vp.empty() && up.empty())
+    TriangulateFromVp(vp_to_triangulate, xyz);
+  else if (!up.empty() && !vp.empty())
+    TriangulateFromUpVp(up_to_triangulate, vp_to_triangulate, xyz);
+
+  // Mask
+  point_cloud = cv::Mat(up.size(), CV_32FC3, cv::Scalar(NAN, NAN, NAN));
+  xyz.copyTo(point_cloud, mask);
+}
+
+void Triangulator::TriangulateFromUp(const cv::Mat &up, cv::Mat &xyz)
+{
+  // Solve for xyzw using determinant tensor
+  cv::Mat C = determinant_tensor_;
+  std::vector<cv::Mat> xyzw(4);
+  for (unsigned int i = 0; i < 4; i++)
+  {
+    //        xyzw[i].create(up.size(), CV_32F);
+    xyzw[i] = xyzw_precompute_offset_[i] + xyzw_precompute_factor_[i].mul(up);
+  }
+
+  // Convert to non homogenous coordinates
+  cv::Mat winv;
+  cv::divide(1.0, xyzw[3], winv);
+  for (unsigned int i = 0; i < 3; i++)
+    xyzw[i] = xyzw[i].mul(winv);
+
+  // Merge
+  cv::merge(std::vector<cv::Mat>(xyzw.begin(), xyzw.begin() + 3), xyz);
+}
+
+void Triangulator::TriangulateFromVp(const cv::Mat &vp, cv::Mat &xyz)
+{
+  // Solve for xyzw using determinant tensor
+  cv::Mat C = determinant_tensor_;
+  std::vector<cv::Mat> xyzw(4);
+  for (unsigned int i = 0; i < 4; i++)
+  {
+    //        xyzw[i].create(vp.size(), CV_32F);
+    xyzw[i] = C.at<float>(cv::Vec4i(i, 0, 1, 1)) - C.at<float>(cv::Vec4i(i, 2, 1, 1)) * uc_ -
+              C.at<float>(cv::Vec4i(i, 0, 2, 1)) * vc_ - C.at<float>(cv::Vec4i(i, 0, 1, 2)) * vp +
+              C.at<float>(cv::Vec4i(i, 2, 1, 2)) * vp.mul(uc_) + C.at<float>(cv::Vec4i(i, 0, 2, 2)) * vp.mul(vc_);
+  }
+
+  // Convert to non homogenous coordinates
+  cv::Mat winv;
+  cv::divide(1.0, xyzw[3], winv);
+  for (unsigned int i = 0; i < 3; i++)
+    xyzw[i] = xyzw[i].mul(winv);
+
+  // Merge
+  cv::merge(std::vector<cv::Mat>(xyzw.begin(), xyzw.begin() + 3), xyz);
+}
+
+void Triangulator::TriangulateFromUpVp(const cv::Mat &up, const cv::Mat &vp, cv::Mat &xyz)
+{
+  // TODO: Assign proj_points_proj without cloning (most likely with for loop, which is how copyTo
+  // does it)
+  cv::Mat proj_points_proj(2, number_pixels_, CV_32F);
+  up.clone().reshape(0, 1).copyTo(proj_points_proj.row(0));
+  vp.clone().reshape(0, 1).copyTo(proj_points_proj.row(1));
+
+  cv::Mat xyzw;
+  cv::triangulatePoints(Pc_, Pp_, proj_points_cam_, proj_points_proj, xyzw);
+
+  xyz.create(3, number_pixels_, CV_32F);
+  for (int i = 0; i < number_pixels_; i++)
+  {
+    xyz.at<float>(0, i) = xyzw.at<float>(0, i) / xyzw.at<float>(3, i);
+    xyz.at<float>(1, i) = xyzw.at<float>(1, i) / xyzw.at<float>(3, i);
+    xyz.at<float>(2, i) = xyzw.at<float>(2, i) / xyzw.at<float>(3, i);
+  }
+
+  xyz = xyz.t();
+  xyz = xyz.reshape(3, up.rows);
+}
+
+}  // namespace reconstruction
+}  // namespace sl_sensor
